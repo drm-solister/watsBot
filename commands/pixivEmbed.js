c9d7c9e09e58e3bce45418a3769d969c3b4093ae
contiello <67182906+drm-solister@users.noreply.github.com>
2021-09-21 00:24:18 -0400

    pixiv embed now uses boe tea proxy
    api.pixiv.moe was hosted in japan and was too slow, discord gave up on embedding it

--- commands/pixivEmbed.js
+++ commands/pixivEmbed.js
@@ -27,7 +27,9 @@ module.exports = {
 
                 let firstEmbed = new MessageEmbed();
                 firstEmbed.setColor([135,164,183]);
-                firstEmbed.setImage(result.body.urls.original.replace(/https\:\/\//, 'https://api.pixiv.moe/image/'));
+                //firstEmbed.setImage(result.body.urls.original.replace(/https\:\/\//, 'https://boe-tea-pximg.herokuapp.com/'));
+                firstImgId = result.body.urls.original.match(/https.*\/([0-9]*)\_*.*\.[a-z]{3}/)[1];
+                firstEmbed.setImage('https://boe-tea-pximg.herokuapp.com/'+firstImgId);
 
                 message.channel.send(firstEmbed).then(firstImage => {
                     let numResults = result.body.pageCount;
@@ -61,7 +63,8 @@ module.exports = {
                                 
                                 msg.setTitle(`${i+1}/${numResults}`);
                                 msg.setColor([135,164,183]);
-                                msg.setImage(result.body.urls.original.replace(/https\:\/\//, 'https://api.pixiv.moe/image/').replace(/_p0/, `_p${i}`));
+                                //msg.setImage(result.body.urls.original.replace(/https\:\/\//, 'https://boe-tea-pximg.herokuapp.com/').replace(/_p0/, `_p${i}`));
+                                msg.setImage('https://boe-tea-pximg.herokuapp.com/'+firstImgId+`/${i}`);
                                 message.channel.send(msg).then(something => {
                                     sendithImage(i+1);
                                 });
